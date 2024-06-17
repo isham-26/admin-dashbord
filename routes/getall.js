e36404ea8f88e28cef23fcc7b315d0f57e8f9293
sidhart25 <navneetnishchal1420@gmail.com>
2024-06-17 23:11:33 +0530

    api updated for insight page

--- routes/getall.js
+++ routes/getall.js
@@ -4,6 +4,7 @@ import {
   createReport,
   getAllBlogs,
   getAllReports,
+  getBlog,
   getRep,
 } from "../controllers/upload.js";
 import { verifyUser } from "../utils/verifyTokens.js";
@@ -13,6 +14,7 @@ const router = express.Router();
 router.get("/report", getAllReports);
 router.get("/reports", getRep);
 router.get("/blogs", getAllBlogs);
+router.get("/blog", getBlog);
 //router.post("/uploadblog", verifyUser, createBlog);
 
 export default router;
